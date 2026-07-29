36510816f79a2006c624706e95d25634b6c1a4e8
KeplerFonx <momchilthesoldier@gmail.com>
2026-07-29 13:18:04 +0300

    reduce amount of points in polylines by a factor of 3

--- server/constants.js
+++ server/constants.js
@@ -7,26 +7,26 @@ export const NORAD_IDS = Object.freeze({
 });
 
 export const ISS = Object.freeze({
-  ORBIT_POINTS: 1150,
-  PERIOD_BETWEEN_POINTS: 5,
+  ORBIT_POINTS: 380,
+  PERIOD_BETWEEN_POINTS: 15,
 });
 
 export const HUBBLE = Object.freeze({
-  ORBIT_POINTS: 1200,
-  PERIOD_BETWEEN_POINTS: 5,
+  ORBIT_POINTS: 400,
+  PERIOD_BETWEEN_POINTS: 15,
 });
 
 export const CSS = Object.freeze({
-  ORBIT_POINTS: 1150,
-  PERIOD_BETWEEN_POINTS: 5,
+  ORBIT_POINTS: 380,
+  PERIOD_BETWEEN_POINTS: 15,
 });
 
 export const BGS = Object.freeze({
-  ORBIT_POINTS: 200,
-  PERIOD_BETWEEN_POINTS: 5,
+  ORBIT_POINTS: 70,
+  PERIOD_BETWEEN_POINTS: 50,
 });
 
 export const ACS = Object.freeze({
-  ORBIT_POINTS: 1150,
-  PERIOD_BETWEEN_POINTS: 5,
+  ORBIT_POINTS: 380,
+  PERIOD_BETWEEN_POINTS: 15,
 });
