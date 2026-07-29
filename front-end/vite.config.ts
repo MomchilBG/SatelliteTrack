7db9690e7b79f47a7556bce0d2cfd1afbaa6c273
Momchil Ivanov <ivanov.momchil.bg@gmail.com>
2026-07-29 18:45:32 +0300

    set vite config file so the project is buildable

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      supported: {
+        'top-level-await': true,
+      },
+      target: 'esnext',
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es',
+      },
+    },
+  },
   plugins: [react()],
-})
+  worker: {
+    format: 'es',
+  },
+});
