a0a1ed8036d57efae2429d62b1e43bcb14a4f11f
KeplerFonx <momchilthesoldier@gmail.com>
2026-08-24 19:22:35 +0300

    add id, style and disabled properties to button

--- front-end/src/Components/Button/Button.tsx
+++ front-end/src/Components/Button/Button.tsx
@@ -1,20 +1,30 @@
+import type { CSSProperties } from 'react';
 import './Button.css';
 
 const Button = ({
   onClick,
   onClickArgs = [],
-  className,
+  id = '',
+  className = 'custom-button',
   content = '',
+  style = {},
+  disabled = false,
 }: {
   onClick: (...onClickArgs: unknown[]) => void;
   onClickArgs: unknown[];
+  id: string;
   className: string;
   content: string;
+  style: CSSProperties;
+  disabled: boolean;
 }) => {
   return (
     <button
+      id={id}
       onClick={(event) => onClick(...onClickArgs, event?.currentTarget)}
       className={className}
+      style={style}
+      disabled={disabled}
     >
       {content}
     </button>
