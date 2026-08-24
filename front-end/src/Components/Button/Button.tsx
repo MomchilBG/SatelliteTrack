aeca9e949f956d55b94dcf23085aadcd1c7428e1
KeplerFonx <momchilthesoldier@gmail.com>
2026-08-24 18:14:17 +0300

    add content property to button component

--- front-end/src/Components/Button/Button.tsx
+++ front-end/src/Components/Button/Button.tsx
@@ -2,18 +2,22 @@ import './Button.css';
 
 const Button = ({
   onClick,
-  onClickArgs,
+  onClickArgs = [],
   className,
+  content = '',
 }: {
   onClick: (...onClickArgs: unknown[]) => void;
   onClickArgs: unknown[];
   className: string;
+  content: string;
 }) => {
   return (
     <button
       onClick={(event) => onClick(...onClickArgs, event?.currentTarget)}
       className={className}
-    />
+    >
+      {content}
+    </button>
   );
 };
 
