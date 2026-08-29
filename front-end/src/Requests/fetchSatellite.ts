e522d6ed6810a8cd99b2ef9bc05b80abf10d8a37
KeplerFonx <momchilthesoldier@gmail.com>
2026-08-29 15:11:43 +0300

    turn fetch and post functions into async ones, add argument check

--- front-end/src/Requests/fetchSatellite.ts
+++ front-end/src/Requests/fetchSatellite.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import type { APIResponse } from '../Types/satellite.ts';
 
-export const fetchData = (satellite: string): Promise<APIResponse> =>
-  axios
-    .get<APIResponse>(`http://localhost:5001/${satellite}`, {
+export const fetchData = async (path: string): Promise<APIResponse> => {
+  if (typeof path !== 'string') {
+    throw new Error('path argument must be a string');
+  }
+
+  return await axios
+    .get<APIResponse>(`http://localhost:5001/${path}`, {
       responseType: 'json',
     })
     .then((response) =>
@@ -19,3 +23,4 @@ export const fetchData = (satellite: string): Promise<APIResponse> =>
         message: 'Failed to fetch TLEs',
       };
     });
+};
--- front-end/src/Requests/postSatellite.ts
+++ front-end/src/Requests/postSatellite.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 import type { APIResponse } from '../Types/satellite.ts';
 
-export const postSatellite = (noradID: string): Promise<APIResponse> =>
-  axios
-    .post(`http://localhost:5001/add_sat`, { noradID: noradID })
+export const postSatellite = async (noradID: string): Promise<APIResponse> => {
+  if (typeof noradID !== 'string') {
+    throw new Error('noradID argument must be a string');
+  }
+
+  return await axios
+    .post<APIResponse>(`http://localhost:5001/add_sat`, { noradID: noradID })
     .then((response) => response.data)
     .catch((error) => {
       console.log(`Error posting satellite: ${error}`);
@@ -13,3 +17,4 @@ export const postSatellite = (noradID: string): Promise<APIResponse> =>
         message: 'Post request failed',
       };
     });
+};
